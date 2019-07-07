ce4bbb813f07644883a14eb88b45a0113aeddf13
Sarai <sarai.solis@gmail.com>
2019-07-07 14:36:56 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    // res.send('Hello World');
+
+
+    res.render('home', {
+        nombre: 'Diego'
+
+    });
+
+
+});
+
+app.get('/about', (req, res) => {
+    // res.send('Hello World');
+
+
+    res.render('about');
+
+
+});
+
+
+// app.listen(3000);
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
